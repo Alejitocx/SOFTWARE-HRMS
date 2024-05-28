3dd603f806974b818f75d5c7aff4f1ebf90cf393
Alejitocx <itsalejo12@gmail.com>
2024-05-28 10:50:59 -0500

    CONEXION BASE DE DATOS

---
+++ GestionRecursosHumanos/src/gestionrecursoshumanos/Conexion/Conexion.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestionrecursoshumanos.Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alejo
+ */
+public class Conexion {
+    private final String HOSI = "localhost";
+    private final String PUERTQ = "5432";
+    private final String DB = "RECURSOSHUMANOS";
+    private final String USER = "postgres";
+    private final String CONTRA = "123456";
+    
+    
+   public Connection getConection(){
+        Connection conexion = null;
+        try{
+            Class.forName("org.postgresql.Driver");
+             String url = "jdbc:postgresql://" + HOSI + ":" + PUERTQ + "/" + DB;            conexion = DriverManager.getConnection(url,USER, CONTRA);
+            JOptionPane.showMessageDialog(null,"Conexion Estblecida");
+        }catch (Exception e){
+           JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        return conexion;
+   }
+    
+    
+}
--- GestionRecursosHumanos/src/gestionrecursoshumanos/GestionRecursosHumanos.java
+++ GestionRecursosHumanos/src/gestionrecursoshumanos/GestionRecursosHumanos.java
@@ -4,6 +4,7 @@
  */
 package gestionrecursoshumanos;
 
+import gestionrecursoshumanos.Conexion.Conexion;
 import gestionrecursoshumanos.Vista.Administrador;
 import gestionrecursoshumanos.Vista.Logins;
 import gestionrecursoshumanos.Vista.Usuario;
@@ -20,19 +21,24 @@ public class GestionRecursosHumanos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         SwingUtilities.invokeLater(() -> {
+       //  SwingUtilities.invokeLater(() -> {
              // Crear el JFrame
-             JFrame frame = new JFrame("GESTION DE RECURSOS HUMANOS");
-             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-             frame.setSize(830, 500);
+         //    JFrame frame = new JFrame("GESTION DE RECURSOS HUMANOS");
+         //    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          //   frame.setSize(830, 500);
              
              // Crear el panel Administrador y añadirlo al frame
-             Logins panel = new Logins ();
-             frame.add(panel);
+           //  Logins panel = new Logins ();
+           //  frame.add(panel);
              
              // Hacer visible el frame
-             frame.setVisible(true);
-         });
+            // frame.setVisible(true);
+       //  });
+         
+         Conexion conexion = new Conexion();
+         conexion.getConection();
         
     }
+    
+   
 }
